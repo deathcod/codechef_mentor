f559ae048f89fa31aa279c823ec6c89347a87d14
Chinmay <chinmay.rakshit@gmail.com>
2018-09-24 22:43:16 +0800

    update broadcast message

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,7 +2,11 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast 'chat_channel', message: render_message(message)
+    ActionCable.server.broadcast 'chat_channel', { 
+	message: message.body, 
+        sender_name: message.user.username,
+        time: message.created_at
+	}
   end
 
   private
