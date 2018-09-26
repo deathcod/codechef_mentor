a618cad20248ab677aad76f76043b7bb84b24585
chinmay Rakshit <chinmay@kuvera.in>
2018-09-27 02:20:47 +0530

    render message as json

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,7 +6,8 @@ class ChatController < ApplicationController
 
   # /chats/:id
   def show
-    @messages = Message.where(room_id: params[:id]).order(created_at: :asc)
+    @messages = Message.where(room_id: params[:id]).order(created_at: :asc).map(&:to_hash)
+    render json @messages and return
   end
 
 end
