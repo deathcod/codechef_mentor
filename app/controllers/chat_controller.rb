36553b2796223f1560ea124b4226af6c57cea534
Chinmay <chinmay.rakshit@gmail.com>
2018-09-28 02:25:12 +0800

    fix json isssue

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -7,7 +7,7 @@ class ChatController < ApplicationController
   # /chats/:id
   def show
     @messages = Message.where(room_id: params[:id]).order(created_at: :asc).map(&:to_hash)
-    render json @messages and return
+    render json: @messages and return
   end
 
 end
