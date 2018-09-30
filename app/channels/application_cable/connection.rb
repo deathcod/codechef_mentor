d836f446fc0848b24456498469d825c59ddca154
Chinmay <chinmay.rakshit@gmail.com>
2018-09-30 18:12:20 +0800

    add authentication

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,15 +2,25 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
 
+    APPROVED_AUTH_CODE = "approved_auth_code".freeze
+
     # add validations later
     def connect
-      authenticate_request!
-      self.current_user = User.find_by_username(cookies.signed[:current_user])
+      if authenticated_request?
+        self.current_user = User.find_by_username(cookies.signed[:current_user])
+      else
+	reject_unauthorized_connection
+      end
     end
 
-    protected:
-    def authenticate_request!
-      Rails.logger.info("ApplicationCable::authenticate_request!::success#{request.params[:auth_code]}")
+    def authenticated_request?
+      auth_code = request.params[:auth_code]
+      user_name = cookies.signed[:current_user]
+      if REDIS_POOL.with {|conn| conn.hget(APPROVED_AUTH_CODE, user_name)} == auth_code
+        return true
+      else
+        return false
+      end
     end
   end
 end
